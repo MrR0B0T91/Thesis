9b6cfc30ad7783487999cc24946f87e246c3d2d8
MrRobot <dimabeastpv@gmail.com>
2021-03-23 12:07:57 +0300

    Stage 2 (PostRepository,PostService,PostResponse)

--- src/main/java/main/service/PostService.java
+++ src/main/java/main/service/PostService.java
@@ -1,15 +1,11 @@
 package main.service;
 
 import main.api.response.PostResponse;
-import main.model.ModerationStatus;
 import main.model.Posts;
-import main.model.repositories.PostCommentRepository;
 import main.model.repositories.PostRepository;
-import main.model.repositories.PostVoteRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,29 +14,19 @@ import java.util.List;
 public class PostService {
 
   private final PostRepository postRepository;
-  private final PostVoteRepository postVoteRepository;
-  private final PostCommentRepository postCommentRepository;
-  private Sort sort;
-  private final boolean isActive = true;
-  private List<Posts> postsList;
 
-  public PostService(
-      PostRepository postRepository,
-      PostVoteRepository postVoteRepository,
-      PostCommentRepository postCommentRepository) {
+  private List<Posts> postsList;
+  private PostResponse postResponse = new PostResponse();
+
+  public PostService(PostRepository postRepository) {
     this.postRepository = postRepository;
-    this.postVoteRepository = postVoteRepository;
-    this.postCommentRepository = postCommentRepository;
   }
 
   public PostResponse getPosts(int offset, int limit, String mode) {
 
     if (mode.equals("recent")) {
-      sort = Sort.by("time").descending();
-      Pageable pagingAndSort = PageRequest.of(offset, limit, sort);
-      Page<Posts> recentPage =
-          postRepository.findAllByModerationStatusAndIsActive(
-              ModerationStatus.ACCEPTED, isActive, pagingAndSort);
+      Pageable pagingRecent = PageRequest.of(offset, limit);
+      Page<Posts> recentPage = postRepository.findPostsOrderByRecent(pagingRecent);
       postsList = recentPage.getContent();
     }
     if (mode.equals("popular")) {
@@ -54,15 +40,10 @@ public class PostService {
       postsList = bestPage.getContent();
     }
     if (mode.equals("early")) {
-      sort = Sort.by("time").ascending();
-      Pageable pagingAndSort = PageRequest.of(offset, limit, sort);
-      Page<Posts> earlyPage =
-          postRepository.findAllByModerationStatusAndIsActive(
-              ModerationStatus.ACCEPTED, isActive, pagingAndSort);
+      Pageable pagingEarly = PageRequest.of(offset, limit);
+      Page<Posts> earlyPage = postRepository.findPostsOrderByEarly(pagingEarly);
       postsList = earlyPage.getContent();
     }
-
-    PostResponse postResponse = new PostResponse();
 
     postResponse.setCount(postsList.size());
     postResponse.setPosts(postsList);
