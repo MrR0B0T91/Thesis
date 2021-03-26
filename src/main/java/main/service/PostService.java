9a5470af67eeb6fe61f290ec7629ecb93b2b6265
MrRobot <dimabeastpv@gmail.com>
2021-03-26 16:13:23 +0300

    Stage 2 (Sorting Posts with only one @Query)

--- src/main/java/main/service/PostService.java
+++ src/main/java/main/service/PostService.java
@@ -1,11 +1,15 @@
 package main.service;
 
 import main.api.response.PostResponse;
+import main.converter.PostConverter;
+import main.dto.PostDto;
 import main.model.Posts;
 import main.model.repositories.PostRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.JpaSort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,34 +18,44 @@ import java.util.List;
 public class PostService {
 
   private final PostRepository postRepository;
+  private Sort sort;
+  private final PostConverter postConverter;
 
   private List<Posts> postsList;
+  private List<PostDto> postDtoList;
+
   private PostResponse postResponse = new PostResponse();
 
-  public PostService(PostRepository postRepository) {
+  public PostService(PostRepository postRepository, PostConverter postConverter) {
     this.postRepository = postRepository;
+
+    this.postConverter = postConverter;
   }
 
   public PostResponse getPosts(int offset, int limit, String mode) {
 
     if (mode.equals("recent")) {
-      Pageable pagingRecent = PageRequest.of(offset, limit);
-      Page<Posts> recentPage = postRepository.findPostsOrderByRecent(pagingRecent);
+      sort = Sort.by("time").descending();
+      Pageable pagingRecent = PageRequest.of(offset, limit, sort);
+      Page<Posts> recentPage = postRepository.findAll(pagingRecent);
       postsList = recentPage.getContent();
     }
     if (mode.equals("popular")) {
-      Pageable pagingPopular = PageRequest.of(offset, limit);
-      Page<Posts> popularPage = postRepository.findPostsOrderByLikes(pagingPopular);
+      sort = JpaSort.unsafe(Sort.Direction.DESC,"COUNT(pv1)");
+      Pageable pagingPopular = PageRequest.of(offset, limit, sort);
+      Page<Posts> popularPage = postRepository.findAll(pagingPopular);
       postsList = popularPage.getContent();
     }
     if (mode.equals("best")) {
-      Pageable pagingBest = PageRequest.of(offset, limit);
-      Page<Posts> bestPage = postRepository.findPostsOrderByComments(pagingBest);
+      sort = JpaSort.unsafe(Sort.Direction.ASC,"COUNT(pc)");
+      Pageable pagingBest = PageRequest.of(offset, limit, sort);
+      Page<Posts> bestPage = postRepository.findAll(pagingBest);
       postsList = bestPage.getContent();
     }
     if (mode.equals("early")) {
-      Pageable pagingEarly = PageRequest.of(offset, limit);
-      Page<Posts> earlyPage = postRepository.findPostsOrderByEarly(pagingEarly);
+      sort = Sort.by("time").ascending();
+      Pageable pagingEarly = PageRequest.of(offset, limit, sort);
+      Page<Posts> earlyPage = postRepository.findAll(pagingEarly);
       postsList = earlyPage.getContent();
     }
 
