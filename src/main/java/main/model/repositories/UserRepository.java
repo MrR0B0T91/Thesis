f4ce4de6c967d19d2fe6afc9a1273c05797d8ca4
MrR0B0T91 <dimabeastpv@gmail.com>
2021-04-23 11:23:30 +0600

    Stage 3 api/post/{ID} viewCount+1 isModerator check (second draft)

--- src/main/java/main/model/repositories/UserRepository.java
+++ src/main/java/main/model/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package main.model.repositories;
 
 import main.model.Users;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<Users, Integer> {
+public interface UserRepository extends JpaRepository<Users, Integer> {
+
+  Users findByName(String name);
 }
---
+++ src/main/java/main/springsecurity/UserPrincipal.java
@@ -0,0 +1,62 @@
+package main.springsecurity;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import main.model.Users;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserPrincipal implements UserDetails {
+
+  private Users user;
+
+  public UserPrincipal(Users user) {
+    this.user = user;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    List<GrantedAuthority> authorities = new ArrayList<>();
+    if (user.isModerator()) {
+      GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_MODERATOR");
+      authorities.add(authority);
+    }
+    return authorities;
+  }
+
+  @Override
+  public String getPassword() {
+    return this.user.getPassword();
+  }
+
+  @Override
+  public String getUsername() {
+    return this.user.getName();
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+
+  public boolean isModerator() {
+    return this.user.isModerator();
+  }
+}
