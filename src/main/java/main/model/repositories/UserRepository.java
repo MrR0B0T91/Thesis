9ea6114a83c8ba557ae09f69c083f16fab787a00
MrR0B0T91 <dimabeastpv@gmail.com>
2021-04-26 14:32:01 +0600

    Stage 3 api/post/{ID} viewCount+1 WebSecurityConfig

--- src/main/java/main/model/repositories/UserRepository.java
+++ src/main/java/main/model/repositories/UserRepository.java
@@ -2,10 +2,17 @@ package main.model.repositories;
 
 import main.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<Users, Integer> {
 
-  Users findByName(String name);
+  @Query(
+      "SELECT u "
+          + "FROM Users u "
+          + "WHERE u.name = :name"
+  )
+  Users findByName(@Param("name") String name);
 }
--- src/main/java/main/springsecurity/WebSecurityConfig.java
+++ src/main/java/main/springsecurity/WebSecurityConfig.java
@@ -1,19 +1,18 @@
 package main.springsecurity;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-@EnableWebSecurity
+@Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-  @Bean
-  public UserDetailsService userDetailsService() {
-    InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-    manager.createUser(User.withUsername("user").password("user").roles("USER").build());
-    return manager;
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    auth.inMemoryAuthentication()
+        .withUser("user").password("{noop}password").roles("USER")
+        .and()
+        .withUser("admin").password("{noop}admin").roles("ADMIN");
   }
 }
