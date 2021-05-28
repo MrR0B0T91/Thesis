085dd5b171e33ce4251d27fe6cf023eda197b2a2
MrR0B0T91 <dimabeastpv@gmail.com>
2021-05-28 11:25:32 +0600

    MovieBlog v1.1 (calendar fix)

--- src/main/java/main/service/PostService.java
+++ src/main/java/main/service/PostService.java
@@ -4,7 +4,6 @@ import java.security.Principal;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -165,7 +164,13 @@ public class PostService {
       Calendar postTime = post.getTime();
 
       if (postTime.before(endPoint) && (postTime.after(startPoint))) {
-        posts.put(dateFormat.format(postTime.getTime()), Collections.frequency(yearList, post));
+        String key = dateFormat.format(postTime.getTime());
+        if (posts.containsKey(key)) {
+          posts.put(key,
+              posts.get(key) + 1);
+        } else {
+          posts.put(key, 1);
+        }
       }
     }
 
